50bf866b22752a43902969ea85cb426982ef72cf
Xstar97 <Xstar97@users.noreply.github.com>
2019-03-13 21:37:09 -0400

    Update Local2Server.bat
    Fixed issues with copying and moving....
    Replaced 'copy' command for 'robocopy' instead
    
    added 3 new config options
    
    roboCopyMT="<8-128>"
    roboCopyRetryCount="<1-10000000>"
    roboCopyRetryWaitTime="<X in seconds>"

--- PLEX/Local2Server.bat
+++ PLEX/Local2Server.bat
@@ -1,13 +1,15 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-call set configName=config.txt
-call set logName=log.txt
-call set utilsDir=%CD%\utils
+set configName=config.txt
+set logName=log.txt
+set utilsDir=%CD%\utils
 
 call :getLock
 exit /b
 
+::function loads config file and loops though the specified dir(s) for both TV and Movies
+::additionally starts T.M.M to update the server with .nfo and other media files
 :main
 if not exist "%utilsDir%\" md "%utilsDir%\"
 call :initLogger
@@ -15,6 +17,7 @@ call :config
 
 for /f "delims=" %%x in (%utilsDir%\%configName%) do set "%%x"
 
+IF %ERRORLEVEL% EQU 0 (
 TITLE %title%
 
 %drive2%
@@ -30,17 +33,22 @@ call :localPlexMovies !movieL[%%G]!, !movieS[%%G]!
 call :tmmUpdate
 
 cd %home%
-
 pause
+) else (
+call :logger "exitCode= %ERRORLEVEL%"
+echo "exitCode= %ERRORLEVEL%"
+echo Sorry!, something happened here....
+pause
+)
 exit /b
 
-rem looks for any Shows on local storage and copies it over to the plex server.
+::looks for any Shows on local storage and copies it over to the plex server.
 :localPlexTvShows
 set local=%~1
 set server=%~2
 
-echo "started function :localPlexTvShows - %local%"
-call :logger "started function localPlexTvShows - %local%"
+echo "started function :localPlexTvShows - %local%."
+call :logger "started function localPlexTvShows - %local%."
 cd %local%
 
 echo "looking for files in %local%"
@@ -51,17 +59,16 @@ CALL :fileToFolderName "%%~nf", -, _resultFolderName
 CALL :getSeasonShowNumber "%%~nxf", ret_val2
 call :trim "!ret_val2!", _resultSeasonName
 
-echo "parsing %%f"
-call :logger "parsing %%f"
+echo "parsing %%f..."
+call :logger "parsing %%f..."
 
-call :copyAndMoveFile "!server!\!_resultFolderName!\!_resultSeasonName!", "%%f", "!localBackUp!", "!_resultFolderName!", "%%~nxf"
-
+call :copyAndMoveFile "!server!\!_resultFolderName!\!_resultSeasonName!", "%%~df%%~pf\", "!localBackUp!", "!_resultFolderName!", "%%~nxf"
 )
-echo "finished in %local%"
-call :logger "finished in %local%"
+echo "finished in %local%."
+call :logger "finished in %local%."
 EXIT /B 0
 
-rem looks for any movie on local storage and copies it over to the plex server.
+::looks for any movie on local storage and copies it over to the plex server.
 :localPlexMovies
 set local=%~1
 set server=%~2
@@ -72,27 +79,21 @@ echo "looking for files in %local%"
 call :logger "looking for files in %local%"
 for /r %%f in (*.mp4;*.mkv;*.avi) do (
 
-echo "parsing %%f"
-call :logger "parsing %%f"
+echo "parsing %%~nxf"
+call :logger "parsing %%~nxf"
 
-call :variableContains "%%~nf", (, _stringResult
-if "!_stringResult!"=="0" (
-CALL :fileToFolderName "%%~nf", (, _folderName
-set folderName=!_folderName!
-) else (
 call :trim "%%~nf", _folderName
 set folderName=!_folderName!
-)
 
-call :copyAndMoveFile "!server!\!folderName!", "%%f", "%localBackUp%", "!folderName!", "%%~nxf"
+call :copyAndMoveFile "!server!\!folderName!", "%%~df%%~pf\", %localBackUp%, "!folderName!", "%%~nxf"
 )
-echo "finished in %local%"
-call :logger "finished in %local%"
+echo "finished in %local%."
+call :logger "finished in %local%."
 EXIT /B 0
 
-rem function to copy to the plex server and move the original file to backup.
-rem function to move from the plex server and copy file to backup.
-rem call :copyAndMoveFile "serverDir", "localDir", "backupFolder", "folderName", "fileName"
+::function to copy to the plex server and move the original file to backup.
+::function to move from the plex server and copy file to backup.
+::call :copyAndMoveFile "serverDir", "localDir", "backupFolder", "folderName", "fileName"
 :copyAndMoveFile
 set _serverDir=%~1
 set _localDir=%~2
@@ -100,52 +101,54 @@ set _localBackUpDir=%~3
 set _folderName=%~4
 set _fileName=%~5
 
-if not exist "!_serverDir!\" md "!_serverDir!\" && echo created '!_serverDir!' on PLEX server.  
 if exist "!_serverDir!\!_fileName!" (
 rem server
-echo "'!_fileName!' exists on '!_serverDir!\'"
-call :logger "'!_fileName!' exists on '!_serverDir!\'"
-call :moveFile "!_serverDir!\!_fileName!", "%_localBackUpDir%\Server\!_folderName!", "!_fileName!"
+call :moveFile "%_serverDir%", "%_localBackUpDir%\Server\%_folderName%\", "%_fileName%"
 )
 rem local
 call :copyFile "!_localDir!", "!_serverDir!", "!_fileName!"
-call :moveFile "!_localDir!", "%_localBackUpDir%\Local\!_folderName!", "!_fileName!"
+call :moveFile "%_localDir%", "%_localBackUpDir%\Local\%_folderName%\", "%_fileName%"
 EXIT /B 0
 
+::function to copy file from one location to another
 :copyFile
 set _mDirFileFrom=%~1
 set _mDirFileTo=%~2
 set _mFileName=%~3
 
-Echo n|COPY /z /v "!_mDirFileFrom!" "!_mDirFileTo!\!_mFileName!"
-IF %ERRORLEVEL% EQU 0 (
-echo "copied from '!_mDirFileFrom!' to '!_mDirFileTo!\!_mFileName!'"
-call :logger "copied from '!_mDirFileFrom!' to '!_mDirFileTo!\!_mFileName!'"
+call :logger "copying '%_mFileName%' from '%_mDirFileFrom%' to '%_mDirFileTo%'"
+
+robocopy "%_mDirFileFrom%" "%_mDirFileTo%" "%_mFileName%" /MT:%roboCopyMT% /R:%roboCopyRetryCount% /W:%roboCopyRetryWaitTime% /J /V
+
+IF %ERRORLEVEL% EQU 1 (
+call :logger "copied from '%_mDirFileFrom%\!_mFileName!' to '%_mDirFileTo%\!_mFileName!'"
 ) ELSE (
-echo "Failed to copy '!_mFileName!' to '!_mDirFileTo!\'"
-call :logger "Failed to copy '!_mFileName!' to '!_mDirFileTo!\'"
+call :logger "exitCode= %ERRORLEVEL%"
+call :logger "Copied failed...from '!_mFileName!' to '%_mDirFileTo%'."
 )
 EXIT /B 0
 
+::function to copy file from one location to another and remove the original file
 :moveFile
 set _mDirFileFrom=%~1
-set _mLocalBackUpDir=%~2
+set _mDirFileTo=%~2
 set _mFileName=%~3
 
-if not exist "!_mLocalBackUpDir!" md "!_mLocalBackUpDir!"
+call :logger "moving '!_mFileName!' from '%_mDirFileFrom%\' to '%_mDirFileTo%\"
 
-Echo n|COPY /z /v "!_mDirFileFrom!" "!_mLocalBackUpDir!"
-IF %ERRORLEVEL% EQU 0 (
-del "!_mDirFileFrom!"
-echo "moved from '!_mDirFileFrom!' to '!_mLocalBackUpDir!\!_mFileName!'"
-call :logger "moved from '!_mDirFileFrom!' to '!_mLocalBackUpDir!\!_mFileName!'"
+robocopy "%_mDirFileFrom%" "%_mDirFileTo%\" "%_mFileName%" /MT:%roboCopyMT% /R:%roboCopyRetryCount% /W:%roboCopyRetryWaitTime% /J /V /mov
+
+IF %ERRORLEVEL% EQU 1 (
+call :logger "moved '!_mFileName!' from '%_mDirFileFrom%' to '%_mDirFileTo%'"
 ) ELSE (
-echo "Failed to send '!_mFileName!' to '!_mDirFileTo!\'"
-call :logger "Failed to send '!_mFileName!' to '!_mDirFileTo!\'"
+call :logger "exitCode= %ERRORLEVEL%"
+call :logger "Moved Failed...from '!_mFileName!' to '%_mDirFileTo%'"
 )
 EXIT /B 0
 
-rem output " The Rising of the Shield Hero - S01E001 " = "The Rising of the Shield Hero"
+::call :fileToFolderName "The Rising of the Shield Hero - S01E001.mp4", -, result
+::echo result = !result! 
+::result = "The Rising of the Shield Hero"
 :fileToFolderName
 set fileName=%~1
 set character=%~2
@@ -156,20 +159,25 @@ SET %~3=!_folderName!
 endlocal
 EXIT /B 0
 
+::functions sees if variable has the specific character/word
+:: 0 = true
+:: 1 = false
 :variableContains
 set fileName=%~1
 set character=%~2
 if not "x!fileName:%character%=!"=="x%fileName%" (
-rem = true
     SET %~3=0
 ) else (
-rem = false
     SET %~3=1
 )
 EXIT /B 0
 
 
-rem output S01 = 1, S20 = 20, S00 = Specials
+::call :getSeasonShowNumber "The Rising of the Shield Hero - S01E001.mp4", result
+::call :getSeasonShowNumber "The Rising of the Shield Hero - S00E001.mp4", result2
+::echo result = !result! 
+::result = "S1"
+::result2 = "Specials"
 :getSeasonShowNumber
 set str=%~1
 set "season=%str:- S=" & set "season=%"
@@ -179,8 +187,9 @@ IF %season:~0,1%==0 ( SET %~2=S%season:~1,1% ) ELSE ( SET %~2=S%season:~0,2% )
 )
 EXIT /B 0
 
-rem output " The Rising of the Shield Hero " = "The Rising of the Shield Hero"
-
+::call :trim " The Rising of the Shield Hero ", result
+::echo result = !result! 
+::result = "The Rising of the Shield Hero"
 :trim
 set input=%~1
 for /f "tokens=* delims= " %%a in ("%input%") do set input=%%a
@@ -188,11 +197,11 @@ for /l %%a in (1,1,100) do if "!input:~-1!"==" " set input=!input:~0,-1!
 SET %~2=%input%
 EXIT /B 0
 
-rem calls TinyMediaManager to update the plex server.
+::calls TinyMediaManager to update the plex server.
 :tmmUpdate
-IF exist "%tmmDir%\tinyMediaManagerCMD.exe" (
+IF exist "!tmmDir!\tinyMediaManagerCMD.exe" (
 %drive1%
-cd %tmmDir%
+cd !tmmDir!
 tinyMediaManagerCMD.exe -update -scrapeNew
 %drive2%
 cd %home%
@@ -209,9 +218,13 @@ EXIT /B 0
 
 :logger
 set input=%~1
-echo %input% >> "%utilsDir%\%logName%"
+echo "%input%" >> "%utilsDir%\%logName%"
 EXIT /B 0
 
+
+::function to check if config exists and read it to see if its been properly filled out
+::if config does not exist; it will create a config.txt in the utils\ dir and launch it before
+::excuting the :main fuction
 :config
 if exist %utilsDir%\%configName% (
 echo loading %configName%....
@@ -227,7 +240,7 @@ echo.
 echo drive1="<c drive>"
 echo drive2="<d drive>"
 echo.
-echo home="<home dir where this script is located>"
+echo home="<"%CD%">"
 echo tmmDir="<folder location of Tiny Media Manager>"
 echo localBackUp="<folder to moved copied files to>"
 echo.
@@ -238,6 +251,10 @@ echo.
 echo movieL[0]="<Local Movie folder 1>"
 echo movieS[0]="<Server Movie folder 1>"
 echo movieIndex="<Movie index 0>"
+echo.
+echo roboCopyMT="<8-128>"
+echo roboCopyRetryCount="<1-10000000>"
+echo roboCopyRetryWaitTime="<X in seconds>"
 ) > %utilsDir%\%configName%
 %utilsDir%\%configName%
 echo update the config file before continuing....
